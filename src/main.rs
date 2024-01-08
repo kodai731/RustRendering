c1ee7ba272e8a97b5cf66ff9d8ec966eed94db77
kodai731 <kodai731@gmail.com>
2024-01-08 11:30:12 +0900

    auto format and createing logical device

--- src/main.rs
+++ src/main.rs
@@ -5,28 +5,27 @@
     clippy::unnecessary_wraps
 )]
 
-use anyhow::{anyhow, Result, Ok};
+use anyhow::{anyhow, Ok, Result};
 use log::*;
+use vulkanalia::loader::{LibloadingLoader, LIBRARY};
+use vulkanalia::prelude::v1_2::*;
+use vulkanalia::window as vk_window;
+use vulkanalia::Version;
 use winit::dpi::LogicalSize;
 use winit::event::{Event, WindowEvent};
 use winit::event_loop::{ControlFlow, EventLoop};
 use winit::window::{Window, WindowBuilder};
-use vulkanalia::loader::{LibloadingLoader, LIBRARY};
-use vulkanalia::window as vk_window;
-use vulkanalia::prelude::v1_2::*;
-use vulkanalia::Version;
 const PORTABILITY_MACOS_VERSION: Version = Version::new(1, 3, 216);
 use std::collections::HashSet;
 use std::ffi::CStr;
 use std::os::raw::c_void;
 use vulkanalia::vk::ExtDebugUtilsExtension;
-const VALIDATION_ENABLED: bool =
-    cfg!(debug_assertions);
+const VALIDATION_ENABLED: bool = cfg!(debug_assertions);
 const VALIDATION_LAYER: vk::ExtensionName =
     vk::ExtensionName::from_bytes(b"VK_LAYER_KHRONOS_validation");
 use thiserror::Error;
 
-fn main() -> Result<()>{
+fn main() -> Result<()> {
     pretty_env_logger::init();
 
     // Window
@@ -35,19 +34,23 @@ fn main() -> Result<()>{
         .with_title("Vulkan Tutorial Rust")
         .with_inner_size(LogicalSize::new(1024, 756))
         .build(&event_loop)?;
-    
+
     // App
     let mut app = unsafe { App::create(&window)? };
     let mut destroying = false;
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Poll;
         match event {
-            Event::MainEventsCleared if !destroying =>
-                unsafe { app.render(&window) }.unwrap(),
-            Event::WindowEvent { event: WindowEvent::CloseRequested, ..} => {
+            Event::MainEventsCleared if !destroying => unsafe { app.render(&window) }.unwrap(),
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => {
                 destroying = true;
                 *control_flow = ControlFlow::Exit;
-                unsafe { app.destroy(); }
+                unsafe {
+                    app.destroy();
+                }
             }
             _ => {}
         }
@@ -60,12 +63,14 @@ struct App {
     entry: Entry,
     instance: Instance,
     data: AppData,
+    device: Device,
 }
 
 #[derive(Clone, Debug, Default)]
 struct AppData {
     messenger: vk::DebugUtilsMessengerEXT,
     physical_device: vk::PhysicalDevice,
+    graphics_queue: vk::Queue,
 }
 
 impl App {
@@ -74,7 +79,14 @@ impl App {
         let entry = Entry::new(loader).map_err(|b| anyhow!("{}", b))?;
         let mut data = AppData::default();
         let instance = Self::create_instance(window, &entry, &mut data)?;
-        Ok(Self { entry, instance, data })
+        let _ = pick_physical_device(&instance, &mut data);
+        let device = Self::create_logical_device(&entry, &instance, &mut data)?;
+        Ok(Self {
+            entry,
+            instance,
+            data,
+            device,
+        })
     }
 
     unsafe fn render(&mut self, window: &Window) -> Result<()> {
@@ -82,20 +94,27 @@ impl App {
     }
 
     unsafe fn destroy(&mut self) {
+        self.device.destroy_device(None);
+
         if VALIDATION_ENABLED {
-            self.instance.destroy_debug_utils_messenger_ext(self.data.messenger, None);
+            self.instance
+                .destroy_debug_utils_messenger_ext(self.data.messenger, None);
         }
         self.instance.destroy_instance(None)
     }
 
-    unsafe fn create_instance(window: &Window, entry: &Entry, data: &mut AppData) -> Result<Instance> {
+    unsafe fn create_instance(
+        window: &Window,
+        entry: &Entry,
+        data: &mut AppData,
+    ) -> Result<Instance> {
         let application_info = vk::ApplicationInfo::builder()
             .application_name(b"Vulkan Tutorial\0")
-            .application_version(vk::make_version(1,0,0))
+            .application_version(vk::make_version(1, 0, 0))
             .engine_name(b"No Engine\0")
-            .engine_version(vk::make_version(1,0,0))
-            .api_version(vk::make_version(1,0,0));
-        
+            .engine_version(vk::make_version(1, 0, 0))
+            .api_version(vk::make_version(1, 0, 0));
+
         let mut extensions = vk_window::get_required_instance_extensions(window)
             .iter()
             .map(|e| e.as_ptr())
@@ -106,12 +125,13 @@ impl App {
         }
 
         // for Mac ablability
-        let flags = if
-        cfg!(target_os = "macos") &&
-        entry.version()? >= PORTABILITY_MACOS_VERSION
-        {
+        let flags = if cfg!(target_os = "macos") && entry.version()? >= PORTABILITY_MACOS_VERSION {
             info!("Enabling extensions for macOS portability.");
-            extensions.push(vk::KHR_GET_PHYSICAL_DEVICE_PROPERTIES2_EXTENSION.name.as_ptr());
+            extensions.push(
+                vk::KHR_GET_PHYSICAL_DEVICE_PROPERTIES2_EXTENSION
+                    .name
+                    .as_ptr(),
+            );
             extensions.push(vk::KHR_PORTABILITY_ENUMERATION_EXTENSION.name.as_ptr());
             vk::InstanceCreateFlags::ENUMERATE_PORTABILITY_KHR
         } else {
@@ -124,7 +144,7 @@ impl App {
             .map(|l| l.layer_name)
             .collect::<HashSet<_>>();
 
-        if VALIDATION_ENABLED && !available_layers.contains(&VALIDATION_LAYER)  {
+        if VALIDATION_ENABLED && !available_layers.contains(&VALIDATION_LAYER) {
             return Err(anyhow!("Validation layer requested but not supported"));
         }
 
@@ -179,6 +199,44 @@ impl App {
 
         vk::FALSE
     }
+
+    unsafe fn create_logical_device(
+        entry: &Entry,
+        instance: &Instance,
+        data: &mut AppData,
+    ) -> Result<Device> {
+        let indices = QueueFamilyIndices::get(instance, data, data.physical_device)?;
+        let queue_priorities = &[1.0];
+        let queue_info = vk::DeviceQueueCreateInfo::builder()
+            .queue_family_index(indices.graphics)
+            .queue_priorities(queue_priorities);
+
+        let layers = if VALIDATION_ENABLED {
+            vec![VALIDATION_LAYER.as_ptr()]
+        } else {
+            vec![]
+        };
+
+        let mut extensions = vec![];
+        if cfg!(target_os = "macos") && entry.version()? >= PORTABILITY_MACOS_VERSION {
+            extensions.push(vk::KHR_PORTABILITY_SUBSET_EXTENSION.name.as_ptr());
+        }
+
+        let features = vk::PhysicalDeviceFeatures::builder();
+
+        let queue_infos = &[queue_info];
+
+        let info = vk::DeviceCreateInfo::builder()
+            .queue_create_infos(queue_infos)
+            .enabled_layer_names(&layers)
+            .enabled_extension_names(&extensions)
+            .enabled_features(&features);
+
+        let device = instance.create_device(data.physical_device, &info, None)?;
+
+        data.graphics_queue = device.get_device_queue(indices.graphics, 0);
+        Ok(device)
+    }
 }
 
 #[derive(Debug, Error)]
@@ -187,28 +245,39 @@ pub struct SuitabilityError(pub &'static str);
 unsafe fn pick_physical_device(instance: &Instance, data: &mut AppData) -> Result<()> {
     for physical_device in instance.enumerate_physical_devices()? {
         let properties = instance.get_physical_device_properties(physical_device);
-        
+
         if let Err(error) = check_physical_device(instance, data, physical_device) {
-            warn!("Skipping Physical Device (`{}`): {}", properties.device_name, error);
+            warn!(
+                "Skipping Physical Device (`{}`): {}",
+                properties.device_name, error
+            );
         } else {
             info!("Selected Physical Device (`{}`).", properties.device_name);
             data.physical_device = physical_device;
             return Ok(());
         }
     }
-    
+
     Err(anyhow!("Failed to find suitable physical device"))
 }
 
-unsafe fn check_physical_device(instance: &Instance, data: &AppData, physical_device: vk::PhysicalDevice,) -> Result<()> {
+unsafe fn check_physical_device(
+    instance: &Instance,
+    data: &AppData,
+    physical_device: vk::PhysicalDevice,
+) -> Result<()> {
     let properties = instance.get_physical_device_properties(physical_device);
     if properties.device_type != vk::PhysicalDeviceType::DISCRETE_GPU {
-        return Err(anyhow!(SuitabilityError("Only discrete GPUs are supported")));
+        return Err(anyhow!(SuitabilityError(
+            "Only discrete GPUs are supported"
+        )));
     }
 
     let features = instance.get_physical_device_features(physical_device);
     if features.geometry_shader != vk::TRUE {
-        return Err(anyhow!(SuitabilityError("Missing geometry shader supported")));
+        return Err(anyhow!(SuitabilityError(
+            "Missing geometry shader supported"
+        )));
     }
 
     QueueFamilyIndices::get(instance, data, physical_device)?;
@@ -222,7 +291,11 @@ struct QueueFamilyIndices {
 }
 
 impl QueueFamilyIndices {
-    unsafe fn get(instance: &Instance, data: &AppData, physical_device: vk::PhysicalDevice,) -> Result<Self> {
+    unsafe fn get(
+        instance: &Instance,
+        data: &AppData,
+        physical_device: vk::PhysicalDevice,
+    ) -> Result<Self> {
         let properties = instance.get_physical_device_queue_family_properties(physical_device);
         let graphics = properties
             .iter()
